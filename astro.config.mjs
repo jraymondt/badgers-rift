4a3103596a9665e81785500d6bf73856630b45d9
jraymondt <james@jraymondt.com>
2025-03-26 05:08:15 +0800

    poster

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jraymondt.com',
-	integrations: [mdx(), sitemap()],
-});
+    site: 'https://jraymondt.com',
+    integrations: [mdx(), sitemap()],
+    });
